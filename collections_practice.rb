ff0891f7573debc1e8582785858aed9f73116bf6
Kevin <kevhines@gmail.com>
2021-01-12 23:09:02 -0500

    commit tree 7c5f553800bebbe584d334f3aaa19f7420f0fa66

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,46 @@
+def sort_array_asc(array)
+    array.sort
+end
 
+def sort_array_desc(array)
+    array.sort.reverse
+    #array.sort{ |a, b| b <=> a }
+end
+
+def sort_array_char_count(array)
+    array.sort{ |a, b| a.length <=> b.length }
+end
+
+def swap_elements(array)
+    third = array[2]
+    array[2] = array[1]
+    array[1] = third
+    array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.map do |word|
+        word[2]="$"
+        word
+    end
+end
+
+def find_a(array)
+    array.select do |word|
+        word.start_with?("a")
+    end  
+end
+
+def sum_array(array)
+    array.inject { |result, element| result + element }
+end
+
+def add_s(array)
+    array.each_with_index.collect do |word, index|
+        index != 1 ? word << "s" : word
+    end
+end
